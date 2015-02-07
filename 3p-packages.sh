dca9229b0418a5412bd6681d75b5b0abae294317
Andrew Liu <aliuy8@gmail.com>
2015-02-06 16:42:33 -0800

    Scripts to install packages

---
+++ 3p-packages.sh
@@ -0,0 +1,14 @@
+# Reinstall Node using NodeSource repository
+sudo apt-get remove -y nodejs npm
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
+# Install Sublime Text 3 from WebUpd8 PPA
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update -y
+sudo apt-get install -y sublime-text-installer
+
+# Install Oracle Java 7 from WebUpd8 PPA
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get -y update
+sudo apt-get -y install oracle-java7-installer
---
+++ npm-packages.sh
@@ -0,0 +1,13 @@
+# Update Node
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
+# Update NPM
+sudo npm install -g npm
+
+# Install favorite Node packages
+sudo npm install -g bower express-generator grunt-cli gulp jshint yo
+
+# Update all modules
+sudo npm update -g
