45d4a9f072b84bfceac43fa23c172e374b9849a0
Andrew Liu <aliuy8@gmail.com>
2015-02-17 15:36:40 -0800

    packages.sh - remove node

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 vim screen git nodejs npm chromium-browser
+sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 vim screen git chromium-browser
 
